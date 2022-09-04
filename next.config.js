e2d676c05ff094ddd09f5f9303ef5459ee2f8dc2
Renato Pozzi <renato.pozzi.dev@gmail.com>
2022-09-04 16:45:18 +0200

    added bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 const { i18n } = require("./next-i18next.config");
 
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
   i18n,
   reactStrictMode: true,
   swcMinify: true,
-};
+});
